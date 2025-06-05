b59b1511cfe172d13002f33d910d4e6ea4376b27
naman-sonawane <sonawane.naman14@gmail.com>
2025-06-05 09:52:34 -0400

    fixed some font funnery

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,29 @@
 import type { Metadata } from "next";
-import { Amiri } from "next/font/google";
+import { Amiri, Roboto, Ubuntu, Oswald } from "next/font/google";
 import "./globals.css";
 
 const amiri = Amiri({
   weight: ["400", "700"],
   subsets: ["latin"],
   variable: "--font-amiri",
+});
+
+const roboto = Roboto({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+
+const ubuntu = Ubuntu({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+});
+
+const oswald = Oswald({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-oswald",
 });
 
 export const metadata: Metadata = {
@@ -21,9 +39,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Russo+One&family=Rubik+Mono+One&family=Unna:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Press+Start+2P&family=Rubik+Mono+One&display=swap" 
+          rel="stylesheet" 
+        />
       </head>
-      <body className={`${amiri.variable} font-amiri antialiased`}>
+      <body className={`${amiri.variable} ${roboto.variable} ${ubuntu.variable} ${oswald.variable} font-ubuntu antialiased`}>
         {children}
       </body>
     </html>
